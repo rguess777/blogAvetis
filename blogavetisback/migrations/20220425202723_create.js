d94ebf2476511f79e4bfae95f4907d59ba238e55
rguess777 <98429834+rguess777@users.noreply.github.com>
2022-05-06 08:57:18 +0200

    ajouté les roles

--- blogavetisback/migrations/20220425202723_create.js
+++ blogavetisback/migrations/20220425202723_create.js
@@ -1,34 +1,40 @@
 export const up = async (knex) => {
+  await knex.schema.createTable('roles', (table) => {
+    table.increments('id').notNullable().unique()
+    table.text('label').notNullable()
+  })
 
-    await knex.schema.createTable('users', (table) => {
-        table.increments('id');
-        table.text('email').notNullable().unique();
-        table.text('passwordHash').notNullable();
-        table.text('passwordSalt').notNullable();
-      });
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id')
+    table.text('email').notNullable().unique()
+    table.text('passwordHash').notNullable()
+    table.text('passwordSalt').notNullable()
+    table.integer('roleId').notNullable()
+    table.foreign('roleId').references('id').inTable('roles').onDelete('SET NULL')
+  })
 
-    await knex.schema.createTable('posts', (table) => {
-      table.increments('id');
-      table.text('title').notNullable();
-      table.text('content').notNullable();
-      table.integer('userId').notNullable();
-      table.foreign('userId').references('id').inTable('users');
-      table.datetime('createdAt').notNullable().defaultTo(knex.fn.now());
-    });
-    await knex.schema.createTable('comments', (table) => {
-      table.increments('id');
-      table.text('content').notNullable();
-      table.integer('userId').notNullable();
-      table.integer('postId').notNullable();
-      table.foreign('userId').references('id').inTable('users');
-      table.foreign('postId').references('id').inTable('posts');
-      table.datetime('createdAt').notNullable().defaultTo(knex.fn.now());
-    });
-  };
+  await knex.schema.createTable('posts', (table) => {
+    table.increments('id')
+    table.text('title').notNullable()
+    table.text('content').notNullable()
+    table.integer('userId').notNullable()
+    table.foreign('userId').references('id').inTable('users')
+    table.datetime('createdAt').notNullable().defaultTo(knex.fn.now())
+  })
+  await knex.schema.createTable('comments', (table) => {
+    table.increments('id')
+    table.text('content').notNullable()
+    table.integer('userId').notNullable()
+    table.integer('postId').notNullable()
+    table.foreign('userId').references('id').inTable('users')
+    table.foreign('postId').references('id').inTable('posts')
+    table.datetime('createdAt').defaultTo(knex.fn.now())
+  })
+}
 
-  
-  export const down = async (knex) => {
-    await knex.schema.dropTable('comments');
-    await knex.schema.dropTable('posts');
-    await knex.schema.dropTable('users');
-  };
+export const down = async (knex) => {
+  await knex.schema.dropTable('roles')
+  await knex.schema.dropTable('comments')
+  await knex.schema.dropTable('posts')
+  await knex.schema.dropTable('users')
+}
